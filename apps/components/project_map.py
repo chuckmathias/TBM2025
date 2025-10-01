8a5bf70105c18cb594c98435325ee9483a36c77b
media <media@localhost.localdomain>
2025-10-01 12:08:35 -0600

    Deleted the Open a new Url on the World Map!

--- apps/components/project_map.py
+++ apps/components/project_map.py
@@ -15,37 +15,6 @@ class WorldMapBlock(StructBlock):
         help_text="Display the title above the map"
     )
     
-    # Regional URLs for clickable map areas
-    # north_america_url = URLBlock(
-    #     required=False,
-    #     help_text="URL to navigate to when North America is clicked"
-    # )
-    
-    # south_america_url = URLBlock(
-    #     required=False,
-    #     help_text="URL to navigate to when South America is clicked"
-    # )
-    
-    # europe_url = URLBlock(
-    #     required=False,
-    #     help_text="URL to navigate to when Europe is clicked"
-    # )
-    
-    # africa_url = URLBlock(
-    #     required=False,
-    #     help_text="URL to navigate to when Africa is clicked"
-    # )
-    
-    # asia_url = URLBlock(
-    #     required=False,
-    #     help_text="URL to navigate to when Asia is clicked"
-    # )
-    
-    # oceania_url = URLBlock(
-    #     required=False,
-    #     help_text="URL to navigate to when Oceania is clicked"
-    # )
-    
     # Styling options
     css_class = CharBlock(
         required=False,
