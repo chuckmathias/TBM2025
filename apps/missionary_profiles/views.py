99a20be674a5afd51dd1a111e25b0af5ec8227c0
media <media@localhost.localdomain>
2025-11-17 11:53:17 -0700

    Updated the master view of updates and the linking to the specific profiles

--- apps/missionary_profiles/views.py
+++ apps/missionary_profiles/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from .models import MissionaryUpdatePage
+from .models import MissionaryUpdatePage, MissionaryProfilePage
 
 # Create your views here.
 
 def master_updates_list(request):
+    missionary_slug = request.GET.get('project')
     updates = MissionaryUpdatePage.objects.live().order_by('-first_published_at')
+    missionaries = MissionaryProfilePage.objects.live().order_by('name')
+    missionary = None
+    if missionary_slug:
+        missionary = missionaries.filter(slug=missionary_slug).first()
+        if missionary:
+            updates = updates.filter(missionary_profile=missionary)
     return render(request, 'missionary_profiles/master_updates_list.html', {
         'updates': updates,
+        'missionary': missionary,
+        'missionaries': missionaries,
     })
